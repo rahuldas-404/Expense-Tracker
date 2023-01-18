6dd66158a1897f7f04f8988d577c88dcea2d466f
Rahul <you@example.com>
2023-01-19 00:26:00 +0530

    recycleview with adaptor

--- app/src/main/java/com/example/expensetracker/TransactionAdaptor.kt
+++ app/src/main/java/com/example/expensetracker/TransactionAdaptor.kt
@@ -1,7 +1,10 @@
 package com.example.expensetracker
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class TransactionAdaptor(private val transaction: ArrayList<Transaction>) : RecyclerView.Adapter<TransactionAdaptor.TransactionHolder>() {
@@ -9,4 +12,27 @@ class TransactionAdaptor(private val transaction: ArrayList<Transaction>) : Recy
         val labal: TextView = view.findViewById(R.id.labal);
         val amount: TextView = view.findViewById(R.id.amount);
     }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.transaction, parent, false)
+        return TransactionHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TransactionHolder, position: Int) {
+        val transaction = transaction[position]
+        val context = holder.amount.context
+
+
+        if(transaction.amount >= 0){
+            holder.amount.text = "+ $%.2f".format(transaction.amount)
+            holder.amount.setTextColor(ContextCompat.getColor(context, R.color.green))
+        }else {
+            holder.amount.text = "- $%.2f".format(Math.abs(transaction.amount))
+            holder.amount.setTextColor(ContextCompat.getColor(context, R.color.red))
+        }
+
+        holder.label.text = transaction.label
+    }
+
 }
